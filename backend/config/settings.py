f3de547228f8b6e27b67799e65a0f6d169ac3bb6
Tamercan1 <wawatamercan@gmail.com>
2026-08-22 22:41:31 +0800

    Chained database migrations inside Procfile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--n73h3&po4!lv!^dvi^2xy6qc0&i8hya*58js4285j#w!3%zq#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
