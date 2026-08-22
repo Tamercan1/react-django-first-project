e91afa9feb65ab51400a66659edeb061c96c88ca
Tamercan1 <wawatamercan@gmail.com>
2026-08-22 19:56:03 +0800

    added backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
+const apiUrl = "https://70515d54-d523-4764-b847-412fa03266d6-dev.e1-us-east-azure.choreoapis.dev/react-django-project/backend/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL // backend url
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl // backend url
 });
 
 api.interceptors.request.use(
